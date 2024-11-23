4db56ccd679d2b09296cb5da06be775d674c8a61
Arseniy Kononov <kononovarseniy@gmail.com>
2024-11-23 14:09:22 +0700

    Add documentation for find_cuts

--- src/tilecut/include/tilecut/find_cuts.hpp
+++ src/tilecut/include/tilecut/find_cuts.hpp
@@ -8,6 +8,10 @@
 namespace r7
 {
 
+/// @brief Restores cut segments, i.e. parts of the tile border that belong to the interior of the multipolygon.
+/// @param segments segments of the multipolygon inside the tile.
+/// @param result storage for result segments.
+/// @param tile_size size of the tile.
 void find_cuts(std::span<const Segment2u16> segments, std::vector<Segment2u16> & result, u16 tile_size) noexcept;
 
 } // namespace r7
