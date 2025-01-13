9de87a9ed20401dabc18e54dd99492a5686918d9
Arseniy Kononov <kononovarseniy@gmail.com>
2025-01-13 23:00:22 +0700

    Fix conan package components description

--- conanfile.py
+++ conanfile.py
@@ -6,9 +6,17 @@ from conan.tools.build import check_min_cppstd
 from conan.tools.files import copy
 
 
+def _make_component_name(name):
+    return f"ka_{name}"
+
+
+def _make_target_name(name):
+    return f"ka::{name}"
+
+
 class TilecutRecipe(ConanFile):
     name = "ka_tilecut"
-    version = "0.1"
+    version = "0.1.0"
 
     license = "MIT"
     author = "kononovarseniy@gmail.com"
@@ -32,9 +40,6 @@ class TilecutRecipe(ConanFile):
         copy(self, "kononovarseniy/*", self.recipe_folder, self.export_sources_folder)
         copy(self, "src/*", self.recipe_folder, self.export_sources_folder)
 
-    def validate(self):
-        check_min_cppstd(self, "20")
-
     def requirements(self):
         self.requires("fmt/9.1.0")
 
@@ -46,8 +51,24 @@ class TilecutRecipe(ConanFile):
         if self.options.build_tests or self.options.build_generate_grid:
             self.requires("mpfr/4.2.1")
 
+    def validate(self):
+        check_min_cppstd(self, "20")
+
     def layout(self):
         cmake_layout(self)
+
+        def component_layout(name, path=None):
+            component_name = _make_component_name(name)
+            if path is None:
+                path = ["src", name]
+            # Configuration for editatable package.
+            self.cpp.source.components[component_name].includedirs = [os.path.join(*path, "include")]
+            self.cpp.build.components[component_name].libdirs = [os.path.join(*path)]
+
+        component_layout("common", ["kononovarseniy", "common"])
+        component_layout("geometry_types")
+        component_layout("exact")
+        component_layout("tilecut")
 
     def generate(self):
         deps = CMakeDeps(self)
@@ -69,15 +90,24 @@ class TilecutRecipe(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.components["ka_exact"].libs = ["ka_exact"]
-        self.cpp_info.components["ka_exact"].set_property("cmake_target_name", "ka::exact")
+        def component_info(name, kind, requires=[]):
+            component = self.cpp_info.components[_make_component_name(name)]
+            component.requires = list(map(_make_component_name, requires))
+            component.set_property("cmake_target_name", _make_target_name(name))
 
-        self.cpp_info.components["ka_geometry_types"].bindirs = []
-        self.cpp_info.components["ka_geometry_types"].libdirs = []
-        self.cpp_info.components["ka_geometry_types"].set_property("cmake_target_name", "ka::geometry_types")
+            if kind == "header":
+                component.bindirs = []
+                component.libdirs = []
+            elif kind == "lib":
+                component.libs = [_make_component_name(name)]
+            elif kind == "bin":
+                pass
+            else:
+                assert False
 
-        self.cpp_info.components["ka_tilecut"].libs = ["ka_tilecut"]
-        self.cpp_info.components["ka_tilecut"].set_property("cmake_target_name", "ka::tilecut")
-
+        component_info("common", kind="lib")
+        component_info("geometry_types", kind="header", requires=["common"])
+        component_info("exact", kind="lib", requires=["common", "geometry_types"])
+        component_info("tilecut", kind="lib", requires=["common", "geometry_types"])
         if self.options.build_generate_grid:
-            self.cpp_info.components["ka_generate_grid"].set_property("cmake_target_name", "ka::generate_grid")
+            component_info("generate_grid", kind="bin")
