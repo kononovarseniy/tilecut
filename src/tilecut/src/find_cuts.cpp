94c53ab849d1b633724353f2573b01300f50b047
Arseniy Kononov <kononovarseniy@gmail.com>
2025-02-06 22:25:11 +0700

    Fix find_cuts when segment lies on tile boundary

--- src/tilecut/src/find_cuts.cpp
+++ src/tilecut/src/find_cuts.cpp
@@ -39,13 +39,6 @@ inline namespace
     return std::nullopt;
 }
 
-/// @brief Checks that both points of the segment lie on the same side of tile.
-[[nodiscard]] bool segment_on_boundary(const u16 tile_size, const Segment2u16 segment) noexcept
-{
-    return (segment.a.x == 0 && segment.b.x == 0) || (segment.a.x == tile_size && segment.b.x == tile_size) ||
-           (segment.a.y == 0 && segment.b.y == 0) || (segment.a.y == tile_size && segment.b.y == tile_size);
-}
-
 /// @brief Converts parameter value to coresponding point.
 /// @return Point on the tile boundary.
 [[nodiscard]] Vec2u16 make_point(const u16 tile_size, const u32 parameter) noexcept
@@ -122,6 +115,53 @@ void add_cut(
     return segment_it->a > segment_it->b;
 }
 
+struct TouchingSegment final
+{
+    /// Parameter of the touching_point.
+    u32 parameter;
+    /// Position of the point on the tile boundary.
+    Vec2u16 touching_point;
+    /// Position of the point opposite to one located on the tile boundary.
+    Vec2u16 opposite_point;
+    /// If true, the second point of the original segment is located on the tile boundary.
+    /// This means that some part of the boundary clockwise from the touch point is to the right of the segment,
+    /// or that the segment lies entirely on the boundary.
+    /// For many simple geometries, this means that the next segment of the contour belongs to a different tile,
+    /// hence the name
+    bool outgoing;
+};
+
+/// @brief Checks the precondition of the orientation of segments on the boundary.
+[[nodiscard]] [[maybe_unused]] bool check_orientation_if_on_boundary(
+    const u16 tile_size,
+    const TouchingSegment & touching_segment) noexcept
+{
+    auto a = touching_segment.touching_point;
+    auto b = touching_segment.opposite_point;
+    AR_PRE(a != b);
+    if (touching_segment.outgoing)
+    {
+        std::swap(a, b);
+    }
+    if (a.x == 0 && b.x == 0 && a.y < b.y)
+    {
+        return false;
+    }
+    if (a.x == tile_size && b.x == tile_size && a.y > b.y)
+    {
+        return false;
+    }
+    if (a.y == 0 && b.y == 0 && a.x > b.x)
+    {
+        return false;
+    }
+    if (a.y == tile_size && b.y == tile_size && a.x < b.x)
+    {
+        return false;
+    }
+    return true;
+}
+
 } // namespace
 
 void find_cuts(
@@ -129,18 +169,6 @@ void find_cuts(
     std::vector<Segment2u16> & result,
     const u16 tile_size) noexcept
 {
-    struct TouchingSegment final
-    {
-        /// Parameter of the touching_point.
-        u32 parameter;
-        /// Position of the point on the tile boundary.
-        Vec2u16 touching_point;
-        /// Position of the point opposite to one located on the tile boundary.
-        Vec2u16 opposite_point;
-        /// If true, the second point of the segment is located on the tile boundary.
-        bool outgoing;
-    };
-
     // TODO: Reuse vector.
     std::vector<TouchingSegment> touching_segments;
     touching_segments.reserve(segments.size() * 2);
@@ -215,9 +243,15 @@ void find_cuts(
         std::optional<u32> prev_point;
         const auto process_bunch = [&](const TouchingSegment & cw_segment, const bool repeated_first)
         {
-            const auto previous_boundary_part_is_cut =
-                !cw_segment.outgoing &&
-                !segment_on_boundary(tile_size, { cw_segment.touching_point, cw_segment.opposite_point });
+            AR_PRE(check_orientation_if_on_boundary(tile_size, cw_segment));
+            /// The most clockwise segment of the bunch determines whether the previous part of the boundary
+            /// belongs to the multipolygon.
+            /// When the segment is not on the boundary, the previous part of the boundary is to the right of it and
+            /// therefore does not belong to the polygon.
+            /// When the segment is on the boundary, the precondition above ensures that the outgoing segment lies on
+            /// the previous part of the boundary (which is not a cut since it coincides with the existing segment),
+            /// and the non-outgoing segment lies on the unprocessed part of the boundary.
+            const auto previous_boundary_part_is_cut = !cw_segment.outgoing;
             if (previous_boundary_part_is_cut)
             {
                 if (prev_point.has_value())
