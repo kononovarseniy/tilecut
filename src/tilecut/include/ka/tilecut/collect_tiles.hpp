e44f4222ba25418a8ea40426992e73f4a125fd7a
Arseniy Kononov <kononovarseniy@gmail.com>
2025-02-08 21:31:50 +0700

    Use TileGrid in collect_tiles

--- src/tilecut/include/ka/tilecut/collect_tiles.hpp
+++ src/tilecut/include/ka/tilecut/collect_tiles.hpp
@@ -5,31 +5,32 @@
 
 #include <ka/geometry_types/Segment2.hpp>
 #include <ka/geometry_types/Vec2.hpp>
+#include <ka/tilecut/TileGrid.hpp>
 
 namespace ka
 {
 
 struct Tile final
 {
-    Vec2s32 tile;
+    Vec2s64 tile;
     std::span<const Segment2u16> segments;
 };
 
 /// @brief Groups segments by tile coordinates and abtains the sorted list of tile coordinates.
 /// @note Vector unique_segments is modified by grouping segments by tile coordinates.
+/// @param tile_grid tile grid.
 /// @param unique_segments unique segments of multipolygon.
 /// @param tile_segments container for segments in tile coordinates. Subranges of `tile_segments` are referenced by
 /// items of `tiles` container.
 /// @param tiles container for found tiles. Tiles are sorted by coordinates.
-/// @param tile_size size of the tile.
 /// @note Segments that lie entirely on the boundary of a tile are considered to belong to the tile that is in the left
 /// half-plane relative to that segment.
 /// Thanks to this property, a tile will never contain a 2D part of a polygon.
 /// In addition, this makes it a little easier to find the boundaries of a tile lying inside a polygon.
 void collect_tiles(
+    const TileGrid & tile_grid,
     std::vector<Segment2s64> & unique_segments,
     std::vector<Segment2u16> & tile_segments,
-    std::vector<Tile> & tiles,
-    u16 tile_size) noexcept;
+    std::vector<Tile> & tiles) noexcept;
 
 } // namespace ka
--- src/tilecut/src/collect_tiles.cpp
+++ src/tilecut/src/collect_tiles.cpp
@@ -1,56 +1,16 @@
 #include <algorithm>
 
-#include <ka/common/assert.hpp>
-#include <ka/common/cast.hpp>
-#include <ka/common/fixed.hpp>
+#include <ka/geometry_types/Vec2.hpp>
 #include <ka/tilecut/collect_tiles.hpp>
 
 namespace ka
 {
 
-inline namespace
-{
-
-[[nodiscard]] s32 coordinate_tile(const s64 value, const u16 tile_size)
-{
-    if (value >= 0)
-    {
-        return exact_cast<s32>(value / tile_size);
-    }
-    return exact_cast<s32>(-((-value + tile_size - 1) / tile_size));
-};
-
-/// Returns the coordinates of the tile to which the segment belongs.
-[[nodiscard]] Vec2s32 segment_tile(const Segment2s64 & segment, const u16 tile_size)
-{
-    AR_PRE(segment.a != segment.b);
-    const auto tile_x = std::min(coordinate_tile(segment.a.x, tile_size), coordinate_tile(segment.b.x, tile_size));
-    const auto tile_y = std::min(coordinate_tile(segment.a.y, tile_size), coordinate_tile(segment.b.y, tile_size));
-    // See note in the description of the collect_tiles function.
-    if (segment.a.x == segment.b.x && segment.a.x == tile_x * tile_size)
-    {
-        return {
-            segment.a.y < segment.b.y ? tile_x - 1 : tile_x,
-            tile_y,
-        };
-    }
-    if (segment.a.y == segment.b.y && segment.a.y == tile_y * tile_size)
-    {
-        return {
-            tile_x,
-            segment.a.x < segment.b.x ? tile_y : tile_y - 1,
-        };
-    }
-    return { tile_x, tile_y };
-}
-
-} // namespace
-
 void collect_tiles(
+    const TileGrid & tile_grid,
     std::vector<Segment2s64> & unique_segments,
     std::vector<Segment2u16> & tile_segments,
-    std::vector<Tile> & tiles,
-    const u16 tile_size) noexcept
+    std::vector<Tile> & tiles) noexcept
 {
     tile_segments.clear();
     tiles.clear();
@@ -59,44 +19,33 @@ void collect_tiles(
         return;
     }
 
-    const auto segment_to_tile = [&](const auto & segment)
-    {
-        const auto tile = segment_tile(segment, tile_size);
-        AR_ASSERT(segment.a.x >= tile_size * tile.x);
-        AR_ASSERT(segment.a.x <= tile_size * tile.x + tile_size);
-        AR_ASSERT(segment.a.y >= tile_size * tile.y);
-        AR_ASSERT(segment.a.y <= tile_size * tile.y + tile_size);
-        AR_ASSERT(segment.b.x >= tile_size * tile.x);
-        AR_ASSERT(segment.b.x <= tile_size * tile.x + tile_size);
-        AR_ASSERT(segment.b.y >= tile_size * tile.y);
-        AR_ASSERT(segment.b.y <= tile_size * tile.y + tile_size);
-        return tile;
-    };
-
-    std::ranges::sort(unique_segments, {}, segment_to_tile);
+    std::ranges::sort(
+        unique_segments,
+        {},
+        [&](const auto & segment)
+        {
+            return tile_grid.tile_of(segment);
+        });
 
     tile_segments.reserve(unique_segments.size());
 
-    auto flush_tile = [local_it = tile_segments.begin(), &tiles, &tile_segments](const Vec2s32 tile) mutable
+    auto flush_tile = [local_it = tile_segments.begin(), &tiles, &tile_segments](const Vec2s64 tile) mutable
     {
         tiles.push_back({ .tile = tile, .segments { local_it, tile_segments.end() } });
         local_it = tile_segments.end();
     };
 
-    auto prev_tile = segment_to_tile(unique_segments.front());
+    auto prev_tile = tile_grid.tile_of(unique_segments.front());
 
     for (const auto & segment : unique_segments)
     {
-        const auto tile = segment_to_tile(segment);
+        const auto tile = tile_grid.tile_of(segment);
         if (tile != prev_tile)
         {
             flush_tile(prev_tile);
             prev_tile = tile;
         }
-        tile_segments.push_back({
-            { exact_cast<u16>(segment.a.x - tile.x * tile_size), exact_cast<u16>(segment.a.y - tile.y * tile_size) },
-            { exact_cast<u16>(segment.b.x - tile.x * tile_size), exact_cast<u16>(segment.b.y - tile.y * tile_size) },
-        });
+        tile_segments.push_back(tile_grid.local_coordinates(tile, segment));
     }
     flush_tile(prev_tile);
 }
