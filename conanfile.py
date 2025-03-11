1d2bccc4c5cdf9a9f81ec6cd07a90744191801d9
Arseniy Kononov <kononovarseniy@gmail.com>
2025-03-12 01:09:17 +0700

    Bump version to 0.2.1

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ def _make_target_name(name):
 
 class TilecutRecipe(ConanFile):
     name = "ka_tilecut"
-    version = "0.2.0"
+    version = "0.2.1"
 
     license = "MIT"
     author = "kononovarseniy@gmail.com"
