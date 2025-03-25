1a52b7c53faadccf90c6b6fada37eaa18f526606
Arseniy Kononov <kononovarseniy@gmail.com>
2025-03-25 13:07:31 +0700

    Fix build on some compilers

--- src/tilecut/include/ka/tilecut/lerp_along_segment.hpp
+++ src/tilecut/include/ka/tilecut/lerp_along_segment.hpp
@@ -16,9 +16,9 @@ template <ieee_float F>
     const F stop_value,
     const Vec2<F> position) noexcept
 {
-    const Vec2 start_to_stop { stop.x - start.x, stop.y - start.y };
+    const Vec2<F> start_to_stop { stop.x - start.x, stop.y - start.y };
     const auto start_to_stop_len_sqr = start_to_stop.x * start_to_stop.x + start_to_stop.y * start_to_stop.y;
-    const Vec2 start_to_pos { position.x - start.x, position.y - start.y };
+    const Vec2<F> start_to_pos { position.x - start.x, position.y - start.y };
     const auto t = (start_to_pos.x * start_to_stop.x + start_to_pos.y * start_to_stop.y) / start_to_stop_len_sqr;
     return std::lerp(start_value, stop_value, t);
 }
